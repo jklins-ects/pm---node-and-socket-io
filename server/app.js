e3e8a5d13502ccaaa806abd74c2b1f8eeaef9318
jklins-ects <73347118+jklins-ects@users.noreply.github.com>
2025-05-13 13:12:06 -0400

    initial commit

---
+++ server/app.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const http = require("http");
+const { Server } = require("socket.io");
+
+const app = express();
+const server = http.createServer(app);
+
+const io = new Server(server, {
+    cors: { origin: "*" },
+});
+
+io.on("connection", (socket) => {
+    console.log("User connected: ", socket.id);
+
+    socket.on("message", (msg) => {
+        console.log("Message received ", msg);
+        io.emit("message", msg);
+    });
+});
+
+const PORT = process.env.PORT || 3000;
+server.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
